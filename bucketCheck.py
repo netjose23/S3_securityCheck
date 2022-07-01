22d174c5148e07aa7fae06b6d61c0400e2118b07
jalvarado <jalvarado@approcessing.com>
2022-07-01 16:27:02 -0700

    Finalizing Class Add Ons

--- bucketCheck.py
+++ bucketCheck.py
@@ -1,46 +1,57 @@
-from urllib import response
 import boto3
 from botocore.exceptions import ClientError
 
 
-# get a bucket LIst 
-def getbucketList(id, secret):
-    s3_client = boto3.client('s3', aws_access_key_id=id, aws_secret_access_key=secret)
-    response = s3_client.list_buckets()
-    buckets = []
-    for bucket in response["Buckets"]:
-        buckets += {bucket["Name"]}
-    print(f"This is the available Buckets we can review: {buckets}")
+class s3BucketLIst:
 
-
-# Begin Checking your S3 Bucket 
-def checkBucket(id, secret, buckettoreview):
-    s3_client = boto3.client('s3', aws_access_key_id=id, aws_secret_access_key=secret)
-
-    # Get Public Access Block Configurations
-    aclResponse = s3_client.get_public_access_block(Bucket=buckettoreview)
-    publicPolicy = aclResponse['PublicAccessBlockConfiguration']['BlockPublicPolicy']
-    publicACLs = aclResponse['PublicAccessBlockConfiguration']['BlockPublicAcls']
+    def __init__(self, id, secret):
+        self.id = id
+        self.secret = secret
     
-    # Print out Responses of ACL and Policy settings within PublicAccessBlockConfiguration
-    print(f"For {buckettoreview} The Policy Response is: {publicPolicy}")
-    print(f"For {buckettoreview} ACL response: {publicACLs}")
-    ACLandPolicy = publicACLs and publicPolicy
-    if ACLandPolicy != True:
-        print(f"Bucket {buckettoreview} Policy block and ACLs are set to False so Bucket IS PUBLIC!\n")
-    else:
-        print(f"Bucket {buckettoreview} is NOT Public because Policy and ACL are used\n")
+    def getbucketList(self):
+        s3_client = boto3.client('s3', aws_access_key_id=self.id, aws_secret_access_key=self.secret)
+        response = s3_client.list_buckets()
+        buckets = []
+        for bucket in response["Buckets"]:
+            buckets += {bucket["Name"]}
+        print(f"This is the available Bucket we can review: {buckets}")
 
-    # check Encryption
-    try:
-        # Use Client to Check Encryption of Bucket
-        encryption = s3_client.get_bucket_encryption(Bucket=buckettoreview)
-        encryptionRules = encryption["ServerSideEncryptionConfiguration"]["Rules"]
-        print(f"{buckettoreview} is ENCRYPTED!")
-        print(f"here is Encryption Rules {encryptionRules}")
-    except ClientError as e:
-        if e.response["Error"]["Code"] == 'ServerSideEncryptionConfigurationNotFoundError':
-            print(f"{buckettoreview} is NOT ENCRYPTED!")
-            print(f"This is the Error in ClientError: {e}")
+class s3BucketAccess(s3BucketLIst):
+
+    # Checking your S3 Bucket
+    def accessCheck(self, buckettoreview):
+        self.buckettoreview = buckettoreview
+        s3_client = boto3.client('s3', aws_access_key_id=self.id, aws_secret_access_key=self.secret)
+
+        # get public access block configurations
+        aclResponse = s3_client.get_public_access_block(Bucket=self.buckettoreview)
+        publicPolicy = aclResponse['PublicAccessBlockConfiguration']['BlockPublicPolicy']
+        publicACLs = aclResponse['PublicAccessBlockConfiguration']['BlockPublicAcls']
+
+        # Print out Responses of ACL and Policy settings within PublicAccessBlockConfiguration
+        print(f"For {buckettoreview} The Policy Response is: {publicPolicy}")
+        print(f"For {buckettoreview} ACL response: {publicACLs}")
+        ACLandPolicy = publicACLs and publicPolicy
+        if ACLandPolicy != True:
+            print(f"Bucket {buckettoreview} Policy block and ACLs are set to False so Bucket IS PUBLIC!\n")
         else:
-            print(f"Bucket: unexpected error {buckettoreview}")
+            print(f"Bucket {buckettoreview} is NOT Public because Policy and ACL are used\n")
+        
+class s3Encryption(s3BucketLIst):
+
+    def checkEncryption(self, bucketencryption):
+        self.bucketencryption = bucketencryption
+        self.buckettoreview = self.bucketencryption
+        s3_client = boto3.client('s3', aws_access_key_id=self.id, aws_secret_access_key=self.secret)
+        try:
+            # Use Client to Check Encryption of Bucket
+            encryption = s3_client.get_bucket_encryption(Bucket=self.bucketencryption)
+            encryptionRules = encryption["ServerSideEncryptionConfiguration"]["Rules"]
+            print(f"{self.bucketencryption} is ENCRYPTED!")
+            print(f"here is Encryption Rules {encryptionRules}")
+        except ClientError as e:
+            if e.response["Error"]["Code"] == 'ServerSideEncryptionConfigurationNotFoundError':
+                print(f"{self.bucketencryption} is NOT ENCRYPTED!")
+                print(f"This is the Error in ClientError: {e}")
+            else:
+                print(f"Bucket: unexpected error {self.bucketencryption}")
--- callBucketCheck.py
+++ callBucketCheck.py
@@ -1,10 +1,17 @@
-from bucketCheck import checkBucket, getbucketList
+from bucketCheck import s3BucketLIst, s3BucketAccess, s3Encryption
 
 # You will use your Python Connector IAM user and input the Key ID and Secret Access Key Below
-getbucketList({"aws_access_key_id"}, {"aws_secret_access_key"})
+{variable} = s3BucketLIst({"aws_access_key_id"}, {"aws_secret_access_key"})
+# Output in List format of all your Buckets your IAM user can access
+{variableofAbove}.getbucketList()
 
-"""
-You will pass the same Key Information you used above and include the... 
-Bucket Name you would like to check (within Quotes)
-"""
-checkBucket({"aws_access_key_id"}, {"aws_secret_access_key"},{"YOUR_BUCKET_NAME"})
+# Check S3 Bucket Public Access Configuration - Creating boto3 client inerface
+{Variable} = s3BucketAccess({"aws_access_key_id"}, {"aws_secret_access_key"})
+# Get S3 Bucket of Choice Public Access Configuration
+{variableofAbove}.accessCheck({"YOUR_BUCKET_NAME"})
+
+# Check Encryption 
+# Creating boto3 client interface
+{Variable} = s3Encryption({"aws_access_key_id"}, {"aws_secret_access_key"})
+# Check Encryption for the Bucket you are reviewing
+{variableofAbove}.checkEncryption({"YOUR_BUCKET_NAME"})
