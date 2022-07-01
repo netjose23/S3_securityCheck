9a9c7901cf2d42a3cc6a7d515266a616607dea75
jalvarado <jalvarado@approcessing.com>
2022-07-01 08:58:23 -0700

    Update callBucketCheck.py

--- callBucketCheck.py
+++ callBucketCheck.py
@@ -5,6 +5,6 @@ getbucketList({"aws_access_key_id"}, {"aws_secret_access_key"})
 
 """
 You will pass the same Key Information you used above and include the... 
-Bucket Name you would like to check (within Quotes)
+Bucket Name you would like to check (within Quotes) - Testing Class Implementation
 """
 checkBucket({"aws_access_key_id"}, {"aws_secret_access_key"},{"YOUR_BUCKET_NAME"})
